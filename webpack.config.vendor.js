6a461fe25e06ae9e7c119138adb0fdc847e7b714
JB <jb@boyce.com.au>
2017-11-24 11:37:46 +1100

    Updated NavMenu with active links. Corrected naming convention in webpack.

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -1,52 +1,52 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
-    const extractCSS = new ExtractTextPlugin('vendor.css');
+    const extractCss = new ExtractTextPlugin("vendor.css");
 
     return [{
         stats: { modules: false },
-        resolve: { extensions: [ '.js' ] },
+        resolve: { extensions: [ ".js" ] },
         entry: {
             vendor: [
-                'bootstrap',
-                'bootstrap/dist/css/bootstrap.css',
-                'event-source-polyfill',
-                'isomorphic-fetch',
-                'jquery',
-                'popper.js',
-                'vue',
-                'vue-router'
+                "bootstrap",
+                "bootstrap/dist/css/bootstrap.css",
+                "event-source-polyfill",
+                "isomorphic-fetch",
+                "jquery",
+                "popper.js",
+                "vue",
+                "vue-router"
             ],
         },
         module: {
             rules: [
-                { test: /\.css(\?|$)/, use: extractCSS.extract({ use: isDevBuild ? 'css-loader' : 'css-loader?minimize' }) },
-                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
+                { test: /\.css(\?|$)/, use: extractCss.extract({ use: isDevBuild ? "css-loader" : "css-loader?minimize" }) },
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: "url-loader?limit=100000" }
             ]
         },
         output: { 
-            path: path.join(__dirname, 'wwwroot', 'dist'),
-            publicPath: 'dist/',
-            filename: '[name].js',
-            library: '[name]_[hash]'
+            path: path.join(__dirname, "wwwroot", "dist"),
+            publicPath: "dist/",
+            filename: "[name].js",
+            library: "[name]_[hash]"
         },
         plugins: [
-            extractCSS,
+            extractCss,
             new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery',
-                Popper: ['popper.js', 'default'],
+                $: "jquery",
+                jQuery: "jquery",
+                'window.jQuery': "jquery",
+                Popper: ["popper.js", "default"],
             }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
             }),
             new webpack.DllPlugin({
-                path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-                name: '[name]_[hash]'
+                path: path.join(__dirname, "wwwroot", "dist", "[name]-manifest.json"),
+                name: "[name]_[hash]"
             })
         ].concat(isDevBuild ? [] : [
             new webpack.optimize.UglifyJsPlugin()
